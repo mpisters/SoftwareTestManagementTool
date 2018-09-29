584143f38b79171e5da566a5e54f38bb221d2ca6
michelle <pistersmichelle@gmail.com>
2018-09-29 16:15:18 +0200

    start of test case component

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TestCaseComponent } from './test-case/test-case.component';
+import {MatCardModule, MatFormFieldModule, MatSelectModule} from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestCaseComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
